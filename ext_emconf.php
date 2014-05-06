f3b8bfc776f089960af43c4cc67c31a190918cc1
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-06 14:00:23 +0000

    modifications for static_info_tables 6.0.0 and extbase
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_es/trunk@85106 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "static_info_tables_es"
-#
-# Auto generated 19-12-2006 13:44
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_es".
+ *
+ * Auto generated 14-02-2014 11:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Static Info Tables (es)',
-	'description' => '(es) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'description' => 'Spanish (es) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.0.1',
-	'dependencies' => 'static_info_tables',
+	'version' => '6.0.0',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -27,24 +27,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'RenÃ¯Â¿Â½ Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
-	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'static_info_tables' => '2.0.0-',
-			'php' => '4.1.0-',
-			'typo3' => '4.0-',
+	'author' => 'Romain Hobsterdre',
+	'author_email' => 'hobsty@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'static_info_tables' => '6.0.2-0.0.0',
+			'typo3' => '6.0.6-6.1.99',
 		),
-		'conflicts' => array(
+		'conflicts' =>
+		array (
 		),
-		'suggests' => array(
+		'suggests' =>
+		array (
 		),
-	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"2687";s:20:"class.ext_update.php";s:4:"a0eb";s:21:"ext_conf_template.txt";s:4:"7de5";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"48db";s:14:"ext_tables.php";s:4:"114f";s:14:"ext_tables.sql";s:4:"8dac";s:28:"ext_tables_static_update.sql";s:4:"688a";s:16:"locallang_db.xml";s:4:"be46";}',
-	'suggests' => array(
 	),
 );
 
